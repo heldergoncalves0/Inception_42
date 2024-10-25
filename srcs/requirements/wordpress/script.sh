2f9a55f8ec750a7b951946c1d9c86692dc305613
Hélder Gonçalves <heldergoncalves@MacBook-Pro-de-Helder.local>
2024-10-25 10:42:13 +0100

    almost there

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+while ! mariadb -h$WORDPRESS_DB_HOST -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME &>/dev/null ; do
+	echo "Waiting for MariaDB to start..."
+	sleep 1
+done
+
 # Ensure the directory for WordPress exists
 if [ ! -d /var/www/html ]; then
     mkdir -p /var/www/html
@@ -7,23 +12,33 @@ fi
 
 cd /var/www/html
 
-# Download WordPress core using wp-cli
-if ! command -v wp &> /dev/null; then
-    echo "wp-cli is not installed!"
-    exit 1
+rm -rf *
+
+# Download and install WP-CLI
+if [ ! -f /usr/local/bin/wp ]; then
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
 fi
 
 # Download and configure WordPress
-wp core download --allow-root --version=5.8.2 --locale=pt_PT
+wp core download --allow-root
 
 # Create wp-config.php file
-wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_ADMIN_USER} --dbpass=${MYSQL_ADMIN_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
+wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root
 
 # Install WordPress
-wp core install --url=${DOMAIN_NAME} --title="${WORDPRESS_TITLE}" --admin_user=${MYSQL_ADMIN_USER} --admin_password=${MYSQL_ADMIN_PASSWORD} --admin_email=${WORDPRESS_EMAIL} --allow-root
+wp core install --url="https://helferna.42.fr" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_DB_USER" --admin_password="$WORDPRESS_DB_PASSWORD" --admin_email="$WORDPRESS_DB_EMAIL" --allow-root
 
 # Create a new user
-wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --role=author --user_pass=${WORDPRESS_USER_PASSWORD} --allow-root
+#wp user create "$WORDPRESS_DB_USER" "$WORDPRESS_DB_EMAIL" --role=author --user_pass="$WORDPRESS_DB_PASSWORD" --allow-root
+
+# Install the WordPress theme
+wp theme install twentynineteen --activate --allow-root
+
+# allow PHP-FPM to listen on port 9000
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir -p /run/php
 
 # Start PHP-FPM (adjust this to the correct version installed in the container)
-exec php-fpm8.0 -F -R
+/usr/sbin/php-fpm7.4 -F
